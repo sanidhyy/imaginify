488003c48c1bb5f08ee24dd6058ac83d0b74a900
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-14 09:19:04 +0000

    sidebar implemented

--- components/shared/sidebar.tsx
+++ components/shared/sidebar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { navLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+
+import { Button } from "../ui/button";
 
 export const Sidebar = () => {
   const pathname = usePathname();
@@ -26,7 +28,7 @@ export const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname;
 
                 return (
@@ -53,7 +55,45 @@ export const Sidebar = () => {
                 );
               })}
             </ul>
+
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={cn(
+                      "sidebar-nav_element group",
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    )}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        className={isActive ? "brightness-200" : ""}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl="/" showName />
+              </li>
+            </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Sign in</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
