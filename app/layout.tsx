239a61fad76e7e10c498b44362ded6a79402f519
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-17 16:27:44 +0000

    site info updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 
 import { Toaster } from "@/components/ui/toaster";
+import { siteConfig } from "@/config";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
@@ -13,10 +14,7 @@ const IBMPlex = IBM_Plex_Sans({
   variable: "--font-ibm-plex",
 });
 
-export const metadata: Metadata = {
-  title: "Imaginify",
-  description: "AI-powered image generator.",
-};
+export const metadata: Metadata = siteConfig;
 
 export default function RootLayout({
   children,
