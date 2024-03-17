2a72b06af57c7bb7f695a0293cf322ad04822c33
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-17 07:41:00 +0000

    env keys types added

--- environment.d.ts
+++ environment.d.ts
@@ -5,7 +5,15 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       // mongo db url
-      MONGODB_URL
+      MONGODB_URL: string;
+
+      // clerk webhook secret
+      WEBHOOK_SECRET: string;
+
+      // cloudinary api keys
+      NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+      CLOUDINARY_API_KEY: string;
+      CLOUDINARY_API_SECRET: string;
     }
   }
 }
